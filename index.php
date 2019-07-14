e587624292496d896bee85ae689f24c78731f2fe
AbhishekPrakash4581 <52873521+AbhishekPrakash4581@users.noreply.github.com>
2019-07-14 10:49:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <?php
 echo "Elastic beanstalk with codepipeline with developer tools with migrations";
-  <Usage:https://www.google.com/search?q=ansible&rlz=1C1GCEU_en-gbIN821IN821&oq=ansi&aqs=chrome.1.69i57j0l5.2646j0j7&sourceid=chrome&ie=UTF-8;
+  <Usage;
 ?>
 
 </body>
